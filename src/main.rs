35f0c591a606afe4d21427ed4b4d88fca44650a3
xfcisco <xfc1@pm.me>
2025-01-05 17:49:08 +0200

    set example to FFE * FFE

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use primitive_types::U256;
 use crypto::finite_field::U256FFE;
 
 fn main() {
     const MODULUS: u128 = 71;
     type FFE = U256FFE<MODULUS>;
-    let fe = FFE::new([0xfffaf103, 0x94000fb0, 0x39c3d09f, 0xe4a0a3fe]);
+    
+    let a = FFE::new([0xfffaf113, 0x94000fb0, 0x39c3d09f, 0xe4a0a3fe]);
+    let b = FFE::new([0x1ff498ea, 0x9541c5d1, 0xd6f9a6c0, 0xe6f89d1f]);
 
-    let a = U256([0x1ff498ea, 0x9541c5d1, 0xd6f9a6c0, 0xe6f89d1f]);
-
-    let prod = a * fe;
-    println!("{}", 1);
+    println!("{a:?} * {b:?} = {:?}", a * b);
 }
